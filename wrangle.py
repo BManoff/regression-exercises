67e3685e8abfe6a462e9b39efb7c35e0f99afb6e
Brian Manoff <brian.a.manoff@gmail.com>
2022-07-20 14:01:24 -0700

    worked on explore exercises

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,7 @@
 from pydataset import data # importing librabries
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 import os
 import env
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer
@@ -16,29 +17,26 @@ def get_connection(db, user=env.user, host=env.host, password=env.password):
 
 def get_wrangle_zillow():
     # Get local cached file if it's there
-    filename = "zillow.csv" 
+    #filename = "zillow.csv" 
 
-    if os.path.isfile(filename):
-        return pd.read_csv(filename)
-    else:
+    #if os.path.isfile(filename):
+        #return pd.read_csv(filename)
+    #else:
         # read the SQL query into a dataframe
         df = pd.read_sql(
         ''' 
         SELECT
-
         bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips
-
         FROM 
-
-        properties_2017 
-
-        JOIN 
-        
-        propertylandusetype using (propertyusetypeid)
-
+        properties_2017
+        JOIN
+        predictions_2017 USING (id)
+        JOIN
+        propertylandusetype USING (propertylandusetypeid)
         WHERE
-         
         propertylandusedesc = "Single Family Residential"
+        AND
+        transactiondate like "2017-__-__";
 
         '''
         
@@ -54,6 +52,8 @@ def handle_nulls(df):
       df = df.dropna() #dropping all the na values
 
       return df
+
+
 
 
 def type_change(df):
@@ -112,3 +112,18 @@ def scale_data(train, validate, test, return_scaler=False):
         return scaler, train_scaled, validate_scaled, test_scaled
     else:
         return train_scaled, validate_scaled, test_scaled
+
+
+def plot_categorical_and_continuous_vars(df, categorical_vars, continuous_vars):
+    for count in continuous_vars:
+        for cat in categorical_vars:
+            _, ax = plt.subplots(1,3,figsize=(20,8))
+            print(f'Plots {count} x {cat}')
+            p = sns.stripplot(data = df, x=cat, y=count, ax=ax[0], s=1)
+            p.axhline(df[count].mean())
+            p = sns.boxplot(data = df, x=cat, y = count, ax=ax[1])
+            p.axhline(df[count].mean())
+            p = sns.violinplot(data = df, x=cat, y=count, hue = cat, ax=ax[2])
+            p.axhline(df[count].mean())
+            plt.suptitle(f'{count} by {cat}', fontsize = 18)
+            plt.show()
